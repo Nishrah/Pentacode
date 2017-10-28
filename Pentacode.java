00ad625d346ec08f7abbdc151b0c1560e44a0083
Nishrah <31913916+Nishrah@users.noreply.github.com>
2017-10-28 10:48:33 +0530

    Update Pentacode.java

--- Pentacode.java
+++ Pentacode.java
@@ -1,5 +1,48 @@
-public class Pentacode{
-public static void main (String []args){
-//test commit
-}
+import robocode.*;
+
+public class Pentacode extends AlphaBot{
+	boolean movingForward;
+	public void run() {
+
+		// Loop forever
+		while (true) {
+			
+			ahead(40000);
+			movingForward = true;
+			//wait(new TurnCompleteCondition(this));
+			turnRight(90);
+			//wait(new TurnCompleteCondition(this));
+			turnLeft(180);
+			//wait(new TurnCompleteCondition(this));
+			turnRight(180);			
+		
+    }
+	}
+	
+	public void onScannedRobot(ScannedRobotEvent e) {
+    if(e.getDistance()<100){
+      fire(3);
+    }
+    else{
+      fire(1);
+    }
+	}
+	
+	public void onHitWall(HitWallEvent e) {
+		// Bounce off!
+		reverseDirection();
+	}
+	
+		public void reverseDirection() {
+			if (movingForward) {
+				back(1000);
+				turnRight(250);
+        movingForward = false;
+      } else {
+        ahead(1000);
+        turnLeft(250);
+        movingForward = true;
+      }
+	}  
+	
 }
